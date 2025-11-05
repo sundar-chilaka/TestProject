1bdb673b0b2f282aca4785b5310cfb2cb20fcbde
sundar-chilaka <sundar3.ch@gmail.com>
2025-11-05 16:49:05 +0530

    1.this is test push..........

---
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class TestController {
+	
+	//http://localhost:8080/api/wish
+	@GetMapping("/wish")
+	public ResponseEntity<String> wish(){
+		String msg = "Hello";
+		return new ResponseEntity<>(msg,HttpStatus.OK);
+	}
+	
+	//http://localhost:8080/api
+	@GetMapping
+	public String massage() {
+		return "Hello this is Controller......!";
+	}
+}
